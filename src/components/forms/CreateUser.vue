000e2bd037e168226b7a6881643a59cd1ca98565
vDomas777 <vileikisdomas@outlook.com>
2022-03-26 09:27:01 -0700

    Responsive things

--- src/components/forms/CreateUser.vue
+++ src/components/forms/CreateUser.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col pt-8">
     <div class="flex gap-2 md:gap-4 flex-col md:flex-row items-center">
       <div class="w-1/2">
-        <div class="flex justify-between">
+        <div class="flex justify-between flex-col sm:flex-row">
           <form-label for="firstName" label="First Name" />
           <p
             class="text-red-500 text-xd font-thin"
@@ -21,7 +21,7 @@
         />
       </div>
       <div class="w-1/2">
-        <div class="flex justify-between">
+        <div class="flex justify-between flex-col sm:flex-row">
           <form-label for="lastName" label="Last Name" />
           <p
             class="text-red-500 text-xd font-thin"
@@ -42,7 +42,7 @@
     </div>
     <div class="flex gap-2 md:gap-4 flex-col md:flex-row items-center">
       <div class="w-1/2">
-        <div class="flex justify-between">
+        <div class="flex justify-between flex-col sm:flex-row">
           <form-label for="email" label="Email" />
           <p
             class="text-red-500 text-xd font-thin"
@@ -61,7 +61,7 @@
         />
       </div>
       <div class="w-1/2 relative">
-        <div class="flex justify-between">
+        <div class="flex justify-between flex-col sm:flex-row">
           <form-label for="country" label="Country" />
           <p
             class="text-red-500 text-xd font-thin"
@@ -80,7 +80,7 @@
     </div>
     <div class="flex gap-2 md:gap-4 flex-col md:flex-row items-center">
       <div class="w-1/2">
-        <div class="flex justify-between">
+        <div class="flex justify-between flex-col sm:flex-row">
           <form-label for="address" label="Address" />
           <p
             class="text-red-500 text-xd font-thin"
@@ -102,7 +102,7 @@
       <div class="w-1/2"></div>
     </div>
     <div class="flex gap-2 md:gap-4 flex-col md:flex-row items-center">
-      <div class="w-full px-32 md:px-0 py-2 md:py-0 md:w-1/2">
+      <div class="w-full px-4 sm:px-32 md:px-0 py-2 md:py-0 md:w-1/2">
         <submit-button title="Create User" @click="submitForm" />
       </div>
     </div>
