e2baa61b51328454132fb034e5d030be3271baed
vDomas777 <vileikisdomas@outlook.com>
2022-03-27 13:50:29 -0700

    ++

--- src/pages/Edit.vue
+++ src/pages/Edit.vue
@@ -1,6 +1,6 @@
 <template>
     <default-layout>
-        <!-- <heading title="Edit" :subTitle="`In this page you can edit ${currentUser.firstName} ${currentUser.lastName}...`" /> -->
+        <heading title="Edit" subTitle="In this page you can edit user" /> 
         <edit-user  />
     </default-layout>
 </template>
